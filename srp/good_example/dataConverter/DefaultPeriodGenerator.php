76cbe916b4498c087ce5382ea3d83b31b52b453e
kolossa <kolossa@users.noreply.github.com>
2023-02-17 15:01:18 +0100

    Create DefaultPeriodGenerator.php

---
+++ srp/good_example/dataConverter/DefaultPeriodGenerator.php
@@ -0,0 +1,27 @@
+<?php 
+
+class DefaultPeriodGenerator
+{
+	public function generate(array $foglalasok, \DateTime $startDate, \DateTime $endDate, \DateInterval $periodInterval):array
+	{
+		$period = new DatePeriod($startDate, $periodInterval, $endDate);
+
+		$chart = array(
+			'normal' => array(),
+		);
+		foreach ( $period as $date ) {
+
+			$year = $date->format('Y');
+			$month = (int)$date->format('m') - 1; //ez kell a Date.UTC miatt
+			$day = $date->format('d');
+
+			$key = new HighchartJsExpr('Date.UTC(' . $year . ', ' . $month . ', ' . $day . ')');
+
+			$normal = array();
+			$normal[0] = $key;
+			$chart['normal'][] = $normal;
+		}
+
+		return $chart;
+	}
+}
