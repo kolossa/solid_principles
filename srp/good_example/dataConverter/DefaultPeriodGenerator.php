c24895bdc591050e270d59ca6d1ebf3d4e949276
kolossa <kolossa@users.noreply.github.com>
2023-02-17 15:13:28 +0100

    Update DefaultPeriodGenerator.php

--- srp/good_example/dataConverter/DefaultPeriodGenerator.php
+++ srp/good_example/dataConverter/DefaultPeriodGenerator.php
@@ -2,9 +2,20 @@
 
 class DefaultPeriodGenerator
 {
-	public function generate(\DateTime $startDate, \DateTime $endDate, \DateInterval $periodInterval):array
+	private \DateTime $startDate;
+	private \DateTime $endDate;
+	private \DateInterval $periodInterval;
+	
+	public function __construct(\DateTime $startDate, \DateTime $endDate, \DateInterval $periodInterval)
 	{
-		$period = new DatePeriod($startDate, $periodInterval, $endDate);
+		$this->startDate = $startDate;
+		$this->endDate = $endDate;
+		$this->periodInterval = $periodInterval;
+	}
+	
+	public function generate():array
+	{
+		$period = new DatePeriod($this->startDate, $this->periodInterval, $this->endDate);
 
 		$chart = array(
 			'normal' => array(),
